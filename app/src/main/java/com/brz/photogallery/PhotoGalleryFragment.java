ed3e606574a4507b149de80936a972085cc1d8a5
Bogdan Zhuravlov <arsbogdanus@gmail.com>
2024-08-16 15:02:05 +0300

    Looper, Handler and HandlerThread (Part 2)

--- app/src/main/java/com/brz/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/brz/photogallery/PhotoGalleryFragment.java
@@ -1,8 +1,11 @@
 package com.brz.photogallery;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -34,7 +37,18 @@ public class PhotoGalleryFragment extends Fragment {
         setRetainInstance(true);
         new FetchItemsTask().execute();
 
-        mThumbnailDownloader = new ThumbnailDownloader<>();
+        Handler responseHandler = new Handler();
+        mThumbnailDownloader = new ThumbnailDownloader<>(responseHandler);
+        mThumbnailDownloader.setThumbnailDownloadListener(
+            new ThumbnailDownloader.ThumbnailDownloadListener<PhotoHolder>() {
+                @Override
+                public void onThumbnailDownloaded(PhotoHolder photoHolder,
+                                                  Bitmap bitmap) {
+                    Drawable drawable = new BitmapDrawable(getResources(), bitmap);
+                    photoHolder.bindDrawable(drawable);
+                }
+            }
+        );
         mThumbnailDownloader.start();
         mThumbnailDownloader.getLooper();
         Log.i(TAG, "Background thread started");
@@ -52,6 +66,12 @@ public class PhotoGalleryFragment extends Fragment {
     }
 
     @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mThumbnailDownloader.clearQueue();
+    }
+
+    @Override
     public void onDestroy() {
         super.onDestroy();
         mThumbnailDownloader.quit();
@@ -65,7 +85,8 @@ public class PhotoGalleryFragment extends Fragment {
     }
 
     private class PhotoHolder extends RecyclerView.ViewHolder {
-        private ImageView mItemImageView;;
+        private ImageView mItemImageView;
+        ;
 
         public PhotoHolder(View itemView) {
             super(itemView);
